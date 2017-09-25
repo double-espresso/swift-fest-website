274b61343ffde4e4d3cbdaea19ab7b2afb52b354
Edoardo Torda <edoardo.torda@gmail.com>
2017-09-26 00:15:38 +0200

    • no background animation on mobile version due performance issues • fixed hero subtitle width on mobile

--- web/app/themes/swiftfest/assets/scripts/app.js
+++ web/app/themes/swiftfest/assets/scripts/app.js
@@ -194,44 +194,47 @@ $(document).ready(function() {
 
 /*------------------*/
 
+var screenSize = $( window ).width();
+console.log(screenSize);
+if ((screenSize>768)) {
+  //floating letters
+  var app = {
 
-//floating letters
-var app = {
+    chars: ['[',']','*','+','{','}',';','/','"','#','_','-'],
 
-  chars: ['[',']','*','+','{','}',';','/','"','#','_','-'],
+    init: function () {
+      app.container = document.createElement('div');
+      app.container.className = 'animation-container';
+      //document.body.appendChild(app.container);
+      if ( $( "#body_homepage" ).length ) {
+        document.getElementById('body_homepage').appendChild(app.container);
+      }
+      window.setInterval(app.add, 90);
+    },
 
-  init: function () {
-    app.container = document.createElement('div');
-    app.container.className = 'animation-container';
-    //document.body.appendChild(app.container);
-    if ( $( "#body_homepage" ).length ) {
-      document.getElementById('body_homepage').appendChild(app.container);
-    }
-    window.setInterval(app.add, 90);
-  },
+    add: function () {
+      var element = document.createElement('span');
+      app.container.appendChild(element);
+      app.animate(element);
+    },
 
-  add: function () {
-    var element = document.createElement('span');
-    app.container.appendChild(element);
-    app.animate(element);
-  },
+    animate: function (element) {
+      var character = app.chars[Math.floor(Math.random() * app.chars.length)];
+      var duration = Math.floor(Math.random() * 40) + 1;
+      var offset = Math.floor(Math.random() * (500 - duration * 2)) + 3;
+      var size = 100 + (15 - duration);
+      element.style.cssText = 'right:'+offset+'vw; font-size:'+size+'px;animation-duration:'+duration+'s';
+      element.innerHTML = character;
+      window.setTimeout(app.remove, duration * 1000, element);
+    },
 
-  animate: function (element) {
-    var character = app.chars[Math.floor(Math.random() * app.chars.length)];
-    var duration = Math.floor(Math.random() * 40) + 1;
-    var offset = Math.floor(Math.random() * (500 - duration * 2)) + 3;
-    var size = 100 + (15 - duration);
-    element.style.cssText = 'right:'+offset+'vw; font-size:'+size+'px;animation-duration:'+duration+'s';
-    element.innerHTML = character;
-    window.setTimeout(app.remove, duration * 1000, element);
-  },
+    remove: function (element) {
+      element.parentNode.removeChild(element);
+    },
+  };
 
-  remove: function (element) {
-    element.parentNode.removeChild(element);
-  },
+  document.addEventListener('DOMContentLoaded', app.init);
 };
-
-document.addEventListener('DOMContentLoaded', app.init);
 
 
 /*------------------*/
