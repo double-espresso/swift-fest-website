2612f06193fd695deeae544a4f9cb4219cbcd06b
Edoardo Torda <edoardo.torda@gmail.com>
2017-10-16 01:27:07 +0200

    ticket is complete

--- web/app/themes/swiftfest/partials/partial-tickets/partial-tickets.php
+++ web/app/themes/swiftfest/partials/partial-tickets/partial-tickets.php
@@ -4,42 +4,35 @@
   $tickets_repeater = get_sub_field('ticket');
 ?>
 <section class="tickets" id="tickets_section">
-  <div class="row">
-    <div class="medium-12 columns">
-      <?php if($tickets_title): ?>
-        <h2 class="title tickets_title"><?php echo $tickets_title; ?></h2>
-      <?php endif; ?>
-      <?php if($tickets_subtitle): ?>
-        <h4 class="current-text tickets_subtitle"><?php echo $tickets_subtitle; ?></h4>
-      <?php endif; ?>
-    </div>
-  </div>
-
   <div class="ticket_container"> 
-  <div class="row collapse">
-    <div class="large-12 large-centered columns">
-      <div class="row">
-        <?php if(have_rows('ticket')): ?>
-          <?php while (have_rows('ticket')): the_row(); ?>
-            <?php $tickets_availability = get_sub_field('available'); ?>
-            <div class="large-4 medium-6 columns end">
-              <div class="ticket_card <?php echo ($tickets_availability == 'no') ? 'not_available' : ''; ?><?php echo ($tickets_availability == 'sold_out') ? 'sold_out' : ''; ?>">
-                <div class="title-mini card_title"><?php the_sub_field('title') ?></div>
-                <div class="current-text card_date"><?php the_sub_field('date_range') ?></div>
-                <div class="card_icon" style="background-image: url('<?php the_sub_field('icon') ?>');"></div>
-                <div class="title card_price">$<?php the_sub_field('price') ?></div>
-                <a href="<?php the_sub_field('button_link'); ?>" target="_blank" title="<?php the_sub_field('button_label'); ?>" class="button">
-                  <?php echo ($tickets_availability == 'sold_out') ? 'SOLD OUT' : the_sub_field('button_label'); ?>
-                </a>
-                <?php if($tickets_availability == 'sold_out'): ?>
-                  <div class="sold_out_badge"></div>
-                <?php  endif; ?>
+    <div class="row collapse">
+      <div class="large-12 large-centered columns">
+        <div class="row">
+          <?php if(have_rows('ticket')): ?>
+            <?php while (have_rows('ticket')): the_row(); ?>
+              <?php $tickets_availability = get_sub_field('available'); ?>
+              <div class="large-4 medium-6 columns end">
+                <div class="tickets">              
+                  <div class="ticket_stack <?php echo ($tickets_availability == 'no') ? 'not_available' : ''; ?><?php echo ($tickets_availability == 'sold_out') ? 'sold_out' : ''; ?>"></div>
+                  <div class="ticket_card <?php echo ($tickets_availability == 'no') ? 'not_available' : ''; ?><?php echo ($tickets_availability == 'sold_out') ? 'sold_out' : ''; ?>">
+                    <div class="title card_price">$<?php the_sub_field('price') ?></div>
+                    <div class="title card_title"><?php the_sub_field('title') ?></div>
+                    <div class="current-text card_date"><?php the_sub_field('date_range') ?></div>
+                    <?php if(get_sub_field('button_link')): ?>
+                      <a href="<?php the_sub_field('button_link'); ?>" target="_blank" title="<?php the_sub_field('button_label'); ?>" class="squared-button orange hero_button">
+                        <?php echo ($tickets_availability == 'sold_out') ? 'SOLD OUT' : the_sub_field('button_label'); ?>
+                      </a>
+                    <?php endif; ?>
+                    <?php if($tickets_availability == 'sold_out'): ?>
+                      <div class="sold_out_badge"></div>
+                    <?php  endif; ?>
+                  </div>
+                </div>
               </div>
-            </div>
-          <?php endwhile; ?>
-        <?php  endif; ?>
+            <?php endwhile; ?>
+          <?php  endif; ?>
+        </div>
       </div>
     </div>
-  </div>
   </div>
 </section>
